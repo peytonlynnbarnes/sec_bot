05ca49fdb4ca1d8c6bf8860bc652f2d49da0c5a5
Quil180 <quil180gaming@gmail.com>
2025-01-15 19:34:25 -0500

    lets try again

--- src/sec_bot/launch/launch_sim.launch.py
+++ src/sec_bot/launch/launch_sim.launch.py
@@ -20,18 +20,18 @@ def generate_launch_description():
     set_model_path = SetEnvironmentVariable(
         'GAZEBO_MODEL_PATH', gazebo_model_path)
 
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    # use_ros2_control = LaunchConfiguration('use_ros2_control')
+    # use_robot_localization = LaunchConfiguration('use_robot_localization')
     use_world_file = LaunchConfiguration('use_world_file')
+    # use_gazebo_gui = LaunchConfiguration('use_gazebo_gui')
     world_file = LaunchConfiguration('world_file')
 
     rsp = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory(
                 package_name), 'launch', 'rsp.launch.py'
-        )]), launch_arguments={'use_sim_time': 'true'}.items()
-    )
+        )]), launch_arguments={'use_sim_time': 'true'}.items())
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
@@ -42,7 +42,8 @@ def generate_launch_description():
             'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_path}.items()
     )
 
-    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
+    # Run the spawner node from the gazebo_ros package. The entity name doesn't
+    # really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'robot'],
@@ -50,6 +51,7 @@ def generate_launch_description():
 
     # Launch them all!
     return LaunchDescription([
+        set_model_path,
         rsp,
         gazebo,
         spawn_entity,

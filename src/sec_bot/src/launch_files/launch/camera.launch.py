f5bea7f3b5781f926b979b44e417fd09a6bff382
Quil180 <quil180gaming@gmail.com>
2025-03-15 23:38:23 -0400

    made launch files its own package

---
+++ src/sec_bot/src/launch_files/launch/camera.launch.py
@@ -0,0 +1,25 @@
+import os # temporary for file locations before turning launch scripts to seperate package
+
+from launch import LaunchDescription
+from launch import LaunchConfiguration
+from launch_ros.actions import Node  # importing the nodes in this funny syntax
+
+
+# generates the description of the launch file
+def generate_launch_description():
+    ld = LaunchDescription()  # calling itself
+
+    pkgs_vision = os.path.join(get_package_share_directory("sec_bo"))
+
+
+    use_sim_time = LaunchConfiguration("use_sim_time") # making the simulation track time
+
+    # initializing the talker node
+    vision_node = Node(
+        package="sec_bot_vision",  # the package where everything is located in
+        executable="talker",  # the name of the node proper
+    )
+
+    ld.add_action(vision_node)  # adding it to the list, LINKED LISTSSSSS
+
+    return ld  # returning the description it found

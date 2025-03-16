644f83259fff1a48aa5d9a13b991fd3063b47fbf
peytonlynnbarnes <peyton0330@gmail.com>
2025-03-16 18:46:30 -0400

    added ball_tracker file

---
+++ src/ball_tracker/setup.py
@@ -0,0 +1,30 @@
+
+from setuptools import find_packages, setup
+
+package_name = 'ball_tracker'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ('share/ball_tracker/launch', ['launch/camera.launch.py']),
+    ('share/ball_tracker/launch', ['launch/sim_cam.launch.py']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='peyton',
+    maintainer_email='peyton0330@gmail.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'image_publisher = ball_tracker.image_publisher:main',
+            'image_subscriber = ball_tracker.image_subscriber:main',
+        ],
+    },
+)

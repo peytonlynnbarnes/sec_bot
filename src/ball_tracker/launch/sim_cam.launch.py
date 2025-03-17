df0332780b34c69bab1aa82e3fd6e39423b98850
peytonlynnbarnes <150793521+peytonlynnbarnes@users.noreply.github.com>
2025-03-17 00:42:54 -0400

    Update sim_cam.launch.py

--- src/ball_tracker/launch/sim_cam.launch.py
+++ src/ball_tracker/launch/sim_cam.launch.py
@@ -5,11 +5,11 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='ball_tracker',
-            executable='image_subscriber',  # Ensure your setup.py entry point is correct
+            executable='image_subscriber',  
             name='image_subscriber_node',
             output='screen',
             parameters=[{
-                'image_topic': '/camera/image_raw'  # Use '/camera2/image_raw' if needed
+                'image_topic': '/camera/image_raw'  
             }],
         ),
     ])

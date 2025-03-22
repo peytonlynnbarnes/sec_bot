fa267c232cc7995929bdc6d0b8d281e6804f630e
Quil <quil180gaming@gmail.com>
2025-03-22 18:11:19 -0400

    updated launch arguments, thanks rafeed

--- src/sec_bot/launch/launch_sim.launch.py
+++ src/sec_bot/launch/launch_sim.launch.py
@@ -55,7 +55,10 @@ def generate_launch_description():
         launch_arguments={
             "topic": "robot_description",
             "name": "my_bot",
-            "bridge_name": "launch_sim_config.yaml",
+            "bridge_name": "robot_description_bridge",
+            "config_file": os.path.join(
+                get_package_share_directory("sec_bot"), "config", "launch_sim_config.yaml"
+            ),
         }.items(),
     )
 

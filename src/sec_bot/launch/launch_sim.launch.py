2cd3d10853c28ddb5cc46c4a056fca302eb06358
Quil <quil180gaming@gmail.com>
2025-03-22 18:07:52 -0400

    added bridge_name with nothing in it

--- src/sec_bot/launch/launch_sim.launch.py
+++ src/sec_bot/launch/launch_sim.launch.py
@@ -52,7 +52,7 @@ def generate_launch_description():
                 )
             ]
         ),
-        launch_arguments={"topic": "robot_description", "name": "my_bot"}.items(),
+        launch_arguments={"topic": "robot_description", "name": "my_bot", "bridge_name": ""}.items(),
     )
 
     # Launch them all!

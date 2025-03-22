75dbec82a05d99a663efd4a5cec6c1ba729cbff2
Quil <quil180gaming@gmail.com>
2025-03-22 17:58:40 -0400

    forgot a g

--- src/sec_bot/launch/launch_sim.launch.py
+++ src/sec_bot/launch/launch_sim.launch.py
@@ -35,7 +35,7 @@ def generate_launch_description():
                 os.path.join(
                     get_package_share_directory("ros_gz_sim"),
                     "launch",
-                    "z_sim.launch.py",
+                    "gz_sim.launch.py",
                 )
             ]
         ),

b6677e89ead1ccdfa353c15fead870cb6902fb8c
BigManRaffa <animedudebro@gmail.com>
2025-03-25 02:01:12 -0400

    yeah changed .hpp to .h LOL

--- src/ros2_orb_slam3/include/ros2_orb_slam3/common.hpp
+++ src/ros2_orb_slam3/include/ros2_orb_slam3/common.hpp
@@ -7,13 +7,16 @@
 #include <sensor_msgs/msg/image.hpp>
 #include <cv_bridge/cv_bridge.hpp>
 #include <opencv2/opencv.hpp>
-#include <tf2_ros/transform_broadcaster.hpp>
+#include <tf2_ros/transform_broadcaster.h>
 #include <geometry_msgs/msg/transform_stamped.hpp>
 #include "System.h"
 #include <memory>
 #include <string>
 
 using std::placeholders::_1;
+
+// Package path constant
+const std::string packagePath = "ros2_ws/ros2_test/src/ros2_orb_slam3/";
 
 // ---------------- MONOCULAR MODE ----------------
 class MonocularMode : public rclcpp::Node {
@@ -56,8 +59,10 @@ public:
     ~StereoMode();
 
 private:
+    void experimentSetting_callback(const std_msgs::msg::String &msg);
     void initializeParams();
     void initializeVSLAM();
+    void initializeVSLAM(std::string &configString);
     void Timestep_callback(const std_msgs::msg::Float64 &msg);
     void LeftImg_callback(const sensor_msgs::msg::Image &msg);
     void RightImg_callback(const sensor_msgs::msg::Image &msg);
@@ -65,9 +70,17 @@ private:
 
     std::shared_ptr<ORB_SLAM3::System> pAgent;
     std::string nodeName, vocFilePath, settingsFilePath, experimentConfig, homeDir;
+    std::string subexperimentconfigName;
+    std::string pubconfigackName;
+    std::string subLeftImgMsgName;
+    std::string subRightImgMsgName;
+    std::string subTimestepMsgName;
     std::string leftImageTopic, rightImageTopic;
     double timeStep = 0;
+    bool bSettingsFromPython = false;
 
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr expConfig_subscription_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr configAck_publisher_;
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subLeftImgMsg_subscription_;
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subRightImgMsg_subscription_;
     rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr subTimestepMsg_subscription_;

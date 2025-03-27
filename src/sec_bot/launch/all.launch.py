987f4ff9e35035309c201c03f72202fccfa63e0e
Quil <quil180gaming@gmail.com>
2025-03-27 18:01:39 -0400

    pushing peytons code

--- src/sec_bot/launch/all.launch.py
+++ src/sec_bot/launch/all.launch.py
@@ -1,36 +1,44 @@
+import launch
+from launch.actions import IncludeLaunchDescription, ExecuteProcess
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
 import os
 
-from ament_index_python.packages import get_package_share_directory
-from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-
 def generate_launch_description():
-    # Get the share directory of the sec_bot package
-    package_share_directory = get_package_share_directory('sec_bot')
-    launch_dir = os.path.join(package_share_directory, 'launch')
-
-    # Define each launch file to include
-    ball_tracker_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(launch_dir, 'ball_tracker_launch.py'))
+    # Paths to the launch files
+    sec_bot_launch_file = os.path.join(
+        get_package_share_directory('sec_bot'),
+        'launch',
+        'launch_sim_model.launch.py'
     )
-    launch_sim = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(launch_dir, 'launch_sim.launch.py'))
+    ball_tracker_launch_file = os.path.join(
+        get_package_share_directory('ball_tracker'),
+        'launch',
+        'sim_cam.launch.py'
     )
-    launch_sim_model = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(launch_dir, 'launch_sim_model.launch.py'))
-    )
-    rsp_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(launch_dir, 'rsp.launch.py'))
+    follow_ball_launch_file = os.path.join(
+        get_package_share_directory('ball_tracker'),
+        'launch',
+        'follow_ball.launch.py'
     )
 
-    # Create the overall launch description
-    ld = LaunchDescription()
-
-    # Add each included launch file to the description
-    ld.add_action(ball_tracker_launch)
-    ld.add_action(launch_sim)
-    ld.add_action(launch_sim_model)
-    ld.add_action(rsp_launch)
-
-    return ld
+    return launch.LaunchDescription([
+        # Include sec_bot launch file
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(sec_bot_launch_file)
+        ),
+        # Include ball_tracker sim_cam launch file
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(ball_tracker_launch_file)
+        ),
+        # Execute ball_tracker sim_multi_ball_tracker node
+        Node(
+            package='ball_tracker',
+            executable='sim_multi_ball_tracker',
+            name='sim_multi_ball_tracker'
+        ),
+        # Include ball_tracker follow_ball launch file
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(follow_ball_launch_file)
+        ),
+    ])

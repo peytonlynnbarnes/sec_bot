6368e921d6c565e3fbd65530befe9314d93058f5
Quil <quil180gaming@gmail.com>
2025-03-27 19:36:18 -0400

    lets try this out

--- src/ball_tracker/launch/orb_slam3_stereo.launch.py
+++ src/ball_tracker/launch/orb_slam3_stereo.launch.py
@@ -4,35 +4,44 @@ from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
 from launch_ros.substitutions import FindPackageShare
 from launch.actions import DeclareLaunchArgument
 
+
 def generate_launch_description():
-    use_sim_time = LaunchConfiguration('use_sim_time')
-    
-    return LaunchDescription([
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='true',
-            description='Use simulation time if true'
-        ),
-        
-        Node(
-            package='ros2_orb_slam3',
-            executable='stereo_node_cpp',
-            name='orb_slam3_stereo',    
-            output='screen',
-            arguments=['--ros-args', '-r', '/orb_slam3/map:=/map'],
-            parameters=[{
-                'vocabulary_file_path': PathJoinSubstitution([
-                    FindPackageShare('ball_tracker'),
-                    'config/Vocabulary/ORBvoc.txt'
-                ]),
-                'settings_file_path': PathJoinSubstitution([
-                    FindPackageShare('ball_tracker'),
-                    'config/stereo.yaml'
-                ]),
-                'use_viewer': True,
-                'use_sim_time': use_sim_time,
-                'left_image_topic': '/camera/left/image_raw',
-                'right_image_topic': '/camera/right/image_raw'
-            }],
-        )
-    ])
+    use_sim_time = LaunchConfiguration("use_sim_time")
+
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                "use_sim_time",
+                default_value="true",
+                description="Use simulation time if true",
+            ),
+            Node(
+                package="ros2_orb_slam3",
+                executable="stereo_node_cpp",
+                name="orb_slam3_stereo",
+                output="screen",
+                arguments=[
+                    PathJoinSubstitution(
+                        [FindPackageShare("ball_tracker"), "config/stereo.yaml"]
+                    ),
+                    "--ros-args",
+                    "-r",
+                    "/orb_slam3/map:=/map",
+                ],
+                parameters=[
+                    {
+                        "vocabulary_file_path": PathJoinSubstitution(
+                            [
+                                FindPackageShare("ball_tracker"),
+                                "config/Vocabulary/ORBvoc.txt",
+                            ]
+                        ),
+                        "use_viewer": True,
+                        "use_sim_time": use_sim_time,
+                        "left_image_topic": "/camera/left/image_raw",
+                        "right_image_topic": "/camera/right/image_raw",
+                    }
+                ],
+            ),
+        ]
+    )

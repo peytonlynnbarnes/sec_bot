74140308be25c75aa54bc88a32756db7e71701b4
Peyton Barnes <peyton0330@gmail.com>
2025-03-27 21:15:48 -0400

    fixed stereo launch file error

--- src/ball_tracker/launch/orb_slam3_stereo.launch.py
+++ src/ball_tracker/launch/orb_slam3_stereo.launch.py
@@ -1,48 +1,40 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
+from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 from launch.actions import DeclareLaunchArgument
 
-
 def generate_launch_description():
-    use_sim_time = LaunchConfiguration("use_sim_time")
+    vocab_path = PathJoinSubstitution([
+        FindPackageShare("ball_tracker"),
+        "config/Vocabulary/ORBvoc.bin"
+    ])
 
-    config_file_path = PathJoinSubstitution([FindPackageShare("ball_tracker"), "config/stereo.yaml"])
+    settings_path = PathJoinSubstitution([
+        FindPackageShare("ball_tracker"),
+        "config/stereo.yaml"
+    ])
 
-    return LaunchDescription(
-        [
-            DeclareLaunchArgument(
-                "use_sim_time",
-                default_value="true",
-                description="Use simulation time if true",
-            ),
-            Node(
-                package="ros2_orb_slam3",
-                executable="stereo_node_cpp",
-                name="orb_slam3_stereo",
-                output="screen",
-                arguments=[
-                    PathJoinSubstitution(
-                        [FindPackageShare("ball_tracker"), "config/stereo.yaml"]
-                    )
-                ],
-                remappings=[("/orb_slam3/map", "/map")],
-                parameters=[
-                    {
-                        "vocabulary_file_path": PathJoinSubstitution(
-                            [
-                                FindPackageShare("ball_tracker"),
-                                "config/Vocabulary/ORBvoc.txt.bin",
-                            ]
-                        ),
-                        "use_viewer": True,
-                        "use_sim_time": use_sim_time,
-                        "left_image_topic": "/camera/left/image_raw",
-                        "right_image_topic": "/camera/right/image_raw",
-                        "config_file_path": config_file_path,
-                    }
-                ],
-            ),
-        ]
-    )
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            "use_sim_time",
+            default_value="true",
+            description="Use simulation time if true",
+        ),
+        Node(
+            package="ros2_orb_slam3",
+            executable="stereo_node_cpp",
+            name="orb_slam3_stereo",
+            output="screen",
+            emulate_tty=True,
+            arguments=[
+                vocab_path,
+                settings_path,
+                "stereo"
+            ],
+            remappings=[
+                ('/camera1/image_raw', '/camera/left/image_raw'),
+                ('/camera2/image_raw', '/camera/right/image_raw')
+            ]
+        )
+    ])
